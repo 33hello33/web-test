21544ea75a5d9dc6902571537d2fa8cc6ede264f
33hello33 <pqviet94@gmail.com>
2022-12-24 11:44:04 +0700

    gen env PORT

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,7 +12,7 @@ func main() {
 	router := gin.Default()
 	router.GET("/", test)
 
-	err := router.Run(":80")
+	err := router.Run(":" + os.Getenv("PORT"))
 	if err != nil {
 		log.Fatal("can not start server " + err.Error())
 	}
